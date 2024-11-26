4827a3d0874cd9e03100c5b5359a8ed0f7ac83fa
Jerry2732002 <jerrysebastian2732002@gmail.com>
2024-11-26 13:12:53 +0530

    ForLoop.java added

--- src/week_two/special_questions/ForLoop.java
+++ src/week_two/special_questions/ForLoop.java
@@ -1,4 +1,10 @@
 package src.week_two.special_questions;
 
 public class ForLoop {
+    public static void main(String[] arg) {
+        System.out.println("Even numbers from 10 to 1");
+        for (int i = 10; i > 0; i = i -2) {
+            System.out.println(i);
+        }
+    }
 }
