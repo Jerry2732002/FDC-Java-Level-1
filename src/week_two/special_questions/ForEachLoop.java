178e90d13d5f1b8661c9615fadab8c7c59e411f0
Jerry2732002 <jerrysebastian2732002@gmail.com>
2024-11-26 13:16:22 +0530

    ForEachLoop.java added

---
+++ src/week_two/special_questions/ForEachLoop.java
@@ -0,0 +1,15 @@
+package src.week_two.special_questions;
+
+public class ForEachLoop {
+
+    public static void main(String[] args) {
+
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+        for(String car : cars) {
+            System.out.println(car);
+        }
+
+    }
+
+}
