38a97032693c7169406747627999fa2244f7f3c5
Jerry2732002 <jerrysebastian2732002@gmail.com>
2024-11-26 14:31:39 +0530

    NestedLoops.java updated

--- src/week_two/special_questions/NestedLoops.java
+++ src/week_two/special_questions/NestedLoops.java
@@ -1,20 +1,17 @@
 package src.week_two.special_questions;
 
 public class NestedLoops {
-    static void printSquare(char row, int col) {
-//        System.out.println("+---+");
-//        System.out.println("| " + row + col + " |");
-//        System.out.print("+---+");
-
-        System.out.print("| " + row + col + " |");
-    }
 
     public static void main(String[] args) {
-        for (char i = 'A'; i <= 'H'; i++) {
-            for (int j = 8; j > 0; j--) {
-                printSquare(i,j);
+        System.out.println("================================================");
+
+        for (int j = 8; j > 0; j--) {
+            for (char i = 'A'; i <= 'H'; i++) {
+                System.out.print("| " + i + j + " |");
             }
             System.out.println();
+            System.out.println("================================================");
+
         }
     }
 }
