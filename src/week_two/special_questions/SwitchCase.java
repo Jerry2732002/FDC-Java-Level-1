9f1aa7e53ec2f8018cb73b29e980f901a96e32be
Jerry2732002 <jerrysebastian2732002@gmail.com>
2024-11-26 14:36:47 +0530

    SwitchCase.java updated

--- src/week_two/special_questions/SwitchCase.java
+++ src/week_two/special_questions/SwitchCase.java
@@ -2,44 +2,29 @@ package src.week_two.special_questions;
 
 import java.util.Scanner;
 
+enum WeekDays {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY;
+}
 
 public class SwitchCase {
-    public enum WeekDays {
-        MONDAY,
-        TUESDAY,
-        WEDNESDAY,
-        THURSDAY,
-        FRIDAY,
-        SATURDAY,
-        SUNDAY;
-    }
 
-    static void getNextDay(WeekDays weekday) {
-        switch (weekday) {
-            case MONDAY:
-                System.out.println(WeekDays.TUESDAY);
-                break;
-            case TUESDAY:
-                System.out.println(WeekDays.WEDNESDAY);
-                break;
-            case WEDNESDAY:
-                System.out.println(WeekDays.THURSDAY);
-                break;
-            case THURSDAY:
-                System.out.println(WeekDays.FRIDAY);
-                break;
-            case FRIDAY:
-                System.out.println(WeekDays.SATURDAY);
-                break;
-            case SATURDAY:
-                System.out.println(WeekDays.SUNDAY);
-                break;
-            case SUNDAY:
-                System.out.println(WeekDays.MONDAY);
-                break;
-            default:
-                System.out.println("Not a Week Day");
-        }
+
+    static WeekDays getNextDay(WeekDays weekday) {
+        return switch (weekday) {
+            case MONDAY -> WeekDays.TUESDAY;
+            case TUESDAY -> WeekDays.WEDNESDAY;
+            case WEDNESDAY -> WeekDays.THURSDAY;
+            case THURSDAY -> WeekDays.FRIDAY;
+            case FRIDAY -> WeekDays.SATURDAY;
+            case SATURDAY -> WeekDays.SUNDAY;
+            case SUNDAY -> WeekDays.MONDAY;
+        };
     }
 
     public static void main(String[] args) {
@@ -50,11 +35,10 @@ public class SwitchCase {
         sc.close();
 
         try {
-            getNextDay(WeekDays.valueOf(weekday));
+            WeekDays nextday = getNextDay(WeekDays.valueOf(weekday));
+            System.out.println("Next Day : " + nextday);
         } catch (IllegalArgumentException e) {
             System.out.println("Not a weekday");
         }
-
-
     }
 }
