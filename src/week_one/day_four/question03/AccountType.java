b91cc12eac28c986f8190690df00170887f19a7c
Jerry2732002 <jerrysebastian2732002@gmail.com>
2024-11-27 17:42:46 +0530

    dayfour question03 added [menu driven to be added]

--- src/week_one/day_four/question03/AccountType.java
+++ src/week_one/day_four/question03/AccountType.java
@@ -4,8 +4,12 @@ public enum AccountType{
     SAVINGS_ACCOUNT(0.5),
     CHECKING_ACCOUNT(0);
 
-    double trasactionFee;
-    AccountType(double trasactionFee){
-        this.trasactionFee = trasactionFee;
+    double transactionFee;
+    AccountType(double transactionFee){
+        this.transactionFee = transactionFee;
+    }
+
+    public double getTransactionFee(){
+        return this.transactionFee;
     }
 }
