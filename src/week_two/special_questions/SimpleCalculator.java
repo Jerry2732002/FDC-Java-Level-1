86b4102019ae017630754575270a997fe3ca810f
Jerry2732002 <jerrysebastian2732002@gmail.com>
2024-11-27 09:09:02 +0530

    SimpleCalculator.java added(working model)

--- src/week_two/special_questions/SimpleCalculator.java
+++ src/week_two/special_questions/SimpleCalculator.java
@@ -4,41 +4,60 @@ import java.util.Scanner;
 
 public class SimpleCalculator {
 
-    private static double twoVariableCalculate(double result, String[] operandOrOperators) {
+    private static double calculate(double result, String[] operandOrOperators) {
+        //Checks if the first character is a digit or operator
+        if (Character.isDigit(operandOrOperators[0].charAt(0))) {
+            result = result + Double.parseDouble(operandOrOperators[0]);
 
-        for (int i = 1; i < operandOrOperators.length; i = i + 2) {
-            switch (operandOrOperators[i]) {
-                case "+":
-                    result += Double.parseDouble(operandOrOperators[i + 1]); break;
-                case "-":
-                    result -= Double.parseDouble(operandOrOperators[i + 1]); break;
-                case "*":
-                    result *= Double.parseDouble(operandOrOperators[i + 1]); break;
-                case "/":
-                    result /= Double.parseDouble(operandOrOperators[i + 1]); break;
+            for (int i = 1; i < operandOrOperators.length; i = i + 2) {
+                switch (operandOrOperators[i]) {
+                    case "+":
+                        result += Double.parseDouble(operandOrOperators[i + 1]);
+                        break;
+                    case "-":
+                        result -= Double.parseDouble(operandOrOperators[i + 1]);
+                        break;
+                    case "*":
+                        result *= Double.parseDouble(operandOrOperators[i + 1]);
+                        break;
+                    case "/":
+                        try {
+                            result /= Double.parseDouble(operandOrOperators[i + 1]);
+                            break;
+                        } catch (ArithmeticException e) {
+                            System.out.println("Cannot divide by zero");
+                        }
+                    default: throw new IllegalArgumentException();
+                }
+
             }
+        } else {
+            for (int i = 0; i < operandOrOperators.length; i = i + 2) {
+                switch (operandOrOperators[i]) {
+                    case "+":
+                        result += Double.parseDouble(operandOrOperators[i + 1]);
+                        break;
+                    case "-":
+                        result -= Double.parseDouble(operandOrOperators[i + 1]);
+                        break;
+                    case "*":
+                        result *= Double.parseDouble(operandOrOperators[i + 1]);
+                        break;
+                    case "/":
+                        try {
+                            result /= Double.parseDouble(operandOrOperators[i + 1]);
+                            break;
+                        } catch (ArithmeticException e) {
+                            System.out.println("Cannot divide by zero");
+                        }
+                    default: throw new IllegalArgumentException();
+                }
 
+            }
         }
         return result;
     }
 
-    private static double oneVariableCalculate(double result, String[] operandOrOperators) {
-
-        for (int i = 0; i < operandOrOperators.length; i = i + 2) {
-            switch (operandOrOperators[i]) {
-                case "+":
-                    result += Double.parseDouble(operandOrOperators[i + 1]); break;
-                case "-":
-                    result -= Double.parseDouble(operandOrOperators[i + 1]); break;
-                case "*":
-                    result *= Double.parseDouble(operandOrOperators[i + 1]); break;
-                case "/":
-                    result /= Double.parseDouble(operandOrOperators[i + 1]); break;
-            }
-
-        }
-        return result;
-    }
 
     public static void main(String[] args) {
         String command;
@@ -51,14 +70,12 @@ public class SimpleCalculator {
             command = sc.nextLine();
 
             String[] operandOrOperators = command.split("\\s");
-            //Checks if the first character is a digit or operator
-            if (Character.isDigit(operandOrOperators[0].charAt(0))) {
-                result = result + Double.parseDouble(operandOrOperators[0]);
-                result = twoVariableCalculate(result, operandOrOperators);
-            }else {
-                result = oneVariableCalculate(result,operandOrOperators);
+            try {
+                result = calculate(result, operandOrOperators);
+                System.out.println("Result : " + result);
+            }catch (Exception e) {
+                System.out.println("WRONG EXPRESSION ENTER AGAIN, e.g. '1.5 + 2.8', or '* 2', or 'exit' to quit");
             }
-            System.out.println("Result : " + result);
         } while (!command.equalsIgnoreCase("exit"));
     }
 }
