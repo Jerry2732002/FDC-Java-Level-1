d838262c75230aebd8e5dd52dc2bf2ff29acf88a
Jerry2732002 <jerrysebastian2732002@gmail.com>
2024-12-11 09:31:04 +0530

    Question11.java updated

--- src/week_three/day_one/Question11.java
+++ src/week_three/day_one/Question11.java
@@ -5,11 +5,21 @@ import java.util.List;
 
 public class Question11 {
     static <T> boolean checkPalindrome(List<T> list) {
-        return list.equals(list.reversed());
+        int size = list.size();
+        int i = size % 2 != 0 ? size / 2 : size / 2 - 1;
+        int j = size / 2;
+        while (i >= 0) {
+            if (!list.get(i).equals(list.get(j))) {
+                return false;
+            }
+            i--;
+            j++;
+        }
+        return true;
     }
 
     public static void execute() {
-        List<Integer> list = new ArrayList<>(List.of(1, 2, 2, 1));
+        List<Integer> list = new ArrayList<>(List.of(1, 2, 1, 1, 2, 1));
         System.out.println(checkPalindrome(list));
     }
 }
